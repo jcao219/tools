0128a0d0cf2d99ff03f046735dc3222e15164b0f
bkc39 <bkc39@cornell.edu>
2014-02-26 20:17:52 -0500

    [cli] Ported the clean command to the Core standard library.
    Conflicts:
    	cs3110-cli/staff/command/clean.ml

--- cs3110-cli/staff/command/clean.ml
+++ cs3110-cli/staff/command/clean.ml
@@ -1,33 +1,50 @@
 open Cli_constants
+open Core.Std
+open Process_util
 
-module P = Process_util
-
-(** [clean ()] remove all files generated during compilation *)
 let build () : unit =
-  P.check_code (Sys.command "ocamlbuild -clean")
+  check_code (Sys.command "ocamlbuild -clean")
 
 let cms () : unit =
-  P.check_code (Sys.command (Format.sprintf "rm -f %s" cCMS_DIR))
+  check_code (Sys.command (Format.sprintf "rm -f %s" cCMS_DIR))
 
 let diff () : unit =
-  P.check_code (Sys.command (Format.sprintf "rm -f %s" cDIFF_RESULTS))
+  check_code (Sys.command (Format.sprintf "rm -f %s" cDIFF_RESULTS))
 
 let email () : unit =
-  P.check_code (Sys.command (Format.sprintf "rm -rf %s" cEMAIL_DIR))
+  check_code (Sys.command (Format.sprintf "rm -rf %s" cEMAIL_DIR))
 
 let harness () : unit =
-  let _ = P.check_code (Sys.command (Format.sprintf "rm -rf %s" cCMS_DIR)) in
-  P.check_code (Sys.command (Format.sprintf "rm -rf %s" cOUTPUT_DIR))
+  let _ = check_code (Sys.command (Format.sprintf "rm -rf %s" cCMS_DIR)) in
+  check_code (Sys.command (Format.sprintf "rm -rf %s" cOUTPUT_DIR))
 
 let smoke () : unit =
-  let () = P.check_code (Sys.command (Format.sprintf "rm -rf %s" cEMAIL_DIR)) in
-  P.check_code (Sys.command (Format.sprintf "rm -rf %s" cNOCOMPILE_DIR))
+  let () = check_code (Sys.command (Format.sprintf "rm -rf %s" cEMAIL_DIR)) in
+  check_code (Sys.command (Format.sprintf "rm -rf %s" cNOCOMPILE_DIR))
 
-let all () : unit =
-  let () = build () in
-  let () = cms () in
-  let () = diff () in
-  let () = email () in
-  let () = harness () in
-  let () = smoke () in
-  ()
+(** [clean ()] removes all files generated during compilation. *)
+let clean () : unit = build   ();
+                      cms     ();
+                      diff    ();
+                      email   ();
+                      harness ();
+                      smoke   ()
+
+let clean_command =
+  Command.basic
+    ~summary:"Removes all of the files generated during compilation."
+    ~readme:(fun () -> String.concat ~sep:"\n" [
+      "The clean command removes all of the automatically generated files and";
+      "directories of the cs3110 tool. These include:";
+      "  1. The build directory from compilation.";
+      "  2. The cms directory for spreadsheets.";
+      "  3. The diff results for resubmissions.";
+      "  4. The emails sent out to no-compiles.";
+      "  5. The test harness results.";
+      "  6. The smoke test results."
+    ])
+    Command.Spec.empty
+    clean
+
+let run_clean_command () =
+  Command.run ~version:"2.0" ~build_info:"Core" clean_command
---
+++ cs3110-cli/staff/command/clean.mli
@@ -0,0 +1,3 @@
+val clean : unit -> unit
+val clean_command : Core.Std.Command.t
+val run_clean_command : unit -> unit
