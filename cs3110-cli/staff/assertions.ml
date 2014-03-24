e201c94be35ceb387964e5567b4c718539c2a967
Ben <ben@rentenna.com>
2014-03-24 00:05:46 -0400

    [staff-cli] merged (===), redefined 'failure' to 'result' for qcheck.

--- cs3110-cli/staff/assertions.ml
+++ cs3110-cli/staff/assertions.ml
@@ -40,6 +40,8 @@ let assert_equal v1 v2 =
       (Serializer.truncate v1)
       (Serializer.truncate v2)))
 
+let (===) v1 v2 = assert_equal (=) v1 v2
+
 exception Almost_equal of string
 let almost_equal v1 v2 =
   let epsilon = 0.0001 in
@@ -120,16 +122,15 @@ let timeout (time : int) (f : 'a -> 'b) (arg : 'a) =
    ignore (Unix.alarm time) ;
    let res = f arg in reset_sigalrm () ; res
 
-exception QCheck_failure of int * string
-let assert_qcheck cases test = 
+exception QCheck_result of int * string
+let assert_qcheck cases test =
   match QCheck.check ~rand:(Random.State.make [|4;2|]) ~n:cNUM_QCHECK cases test with
-  | QCheck.Ok _ -> ()
+  | QCheck.Ok _ -> raise (QCheck_result (0,"All qcheck passed!"))
   | QCheck.Failed [] -> 
     let msg = "qcheck says 'failed', but could not generate a failed instance." in 
-    raise (QCheck_failure (-1, msg)) 
+    raise (QCheck_result (cNUM_QCHECK+1, msg)) 
   | QCheck.Failed (x::xs) -> 
     let num_failed = 1 + List.length xs in
-    let msg = Printf.sprintf "Sample failing instance: '%s'\n" (Serializer.truncate x) in
-    raise (QCheck_failure (num_failed, msg))
+    let msg = Printf.sprintf "Sample failing instance '%s'" (Serializer.truncate x) in
+    raise (QCheck_result (num_failed, msg))
   | QCheck.Error (_, e) -> raise e
-
