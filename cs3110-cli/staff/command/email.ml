c68f87ada0744f49c660fbb80110dcc811dc8db8
Ben <ben@rentenna.com>
2014-06-23 14:20:28 -0400

    [master-doesnt-make] function to generate recipient email address

--- cs3110-cli/staff/command/email.ml
+++ cs3110-cli/staff/command/email.ml
@@ -2,8 +2,10 @@ open Cli_constants
 open Io_util
 open Filepath_util
 
-(** send one email message. Get recipient from the filename, get bcc from the
- * local file of important people. *)
+let get_recipient (msg_file : string) : string =
+  (strip_suffix msg_file) ^ "@cornell.edu"
+
+(** send one email message. *)
 let send_one_email (msg_file : string) (recipient : string) : bool =
   let bcc = Format.sprintf "-b '%s'" (String.concat "' -b '" (read_lines (open_in cEMAIL_ADMINS))) in
   let cmd = Format.sprintf "mutt -s '%s' %s '%s' < %s/%s" cEMAIL_SUBJECT bcc recipient cEMAIL_DIR msg_file in
@@ -14,7 +16,7 @@ let send_one_email (msg_file : string) (recipient : string) : bool =
  * Send one to each student, record and return the number of messages sent. *)
 let send_all_emails () : int =
   Array.fold_left (fun num_sent (msg_file : string) ->
-    let recipient = (strip_suffix msg_file) ^ "@cornell.edu" in
+    let recipient = get_recipient msg_file in
     if send_one_email msg_file recipient then
       num_sent +1
     else 
@@ -24,7 +26,7 @@ let send_all_emails () : int =
 
 (** [email ()] send the email messages stored in the _email directory.  
  * Assumes that every folder name under _email is a valid Cornell netid 
- * Uses [mutt]. You must install [mutt]. *)
+ * Uses the unix utility [mutt]. *)
 let run () : unit = 
   let () = assert_file_exists cEMAIL_DIR in
   let () = assert_file_exists cEMAIL_ADMINS in
