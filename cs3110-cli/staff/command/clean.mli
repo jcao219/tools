a54d108c4965986fab26531e54df2b0bbed60ea1
bkc39 <bkc39@cornell.edu>
2014-06-25 03:07:17 -0400

    [cli#49] Export additional functions.

--- cs3110-cli/staff/command/clean.mli
+++ cs3110-cli/staff/command/clean.mli
@@ -1,3 +1,8 @@
+val build : unit -> unit
+val diff : unit -> unit
+val email : unit -> unit
+val harness : unit -> unit
+val smoke : unit -> unit
 val clean : unit -> unit
 val clean_command : Core.Std.Command.t
 val run_clean_command : unit -> unit
