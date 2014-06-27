7da71e071b021bfb6f589b612029b3b8a79c8644
Ben <ben@rentenna.com>
2014-06-27 01:21:07 -0400

    [tests-folder#29] str_reverse and is_suffix functions

--- cs3110-cli/staff/util/filepath_util.ml
+++ cs3110-cli/staff/util/filepath_util.ml
@@ -7,18 +7,18 @@ let absolute_path (f : string) =
   Format.sprintf "%s/%s" (Sys.getcwd ()) f
 
 (** [split s c b] safely splits string [s] at the left/right-most occurence
- * of character [c]. [b] chooses. If [c] does not appear, returns two 
+ * of character [c]. [b] chooses. If [c] does not appear, returns two
  * copies of [s]. *)
-let split (s : string) (c : char) (b : bool) : string * string = 
+let split (s : string) (c : char) (b : bool) : string * string =
   if not (String.contains s c) then
     (s, s)
-  else 
+  else
     let i = 1 + if b then String.index s c else String.rindex s c in
     let left = String.sub s 0 (i-1) in
-    let right = String.sub s i ((String.length s) - i) in 
+    let right = String.sub s i ((String.length s) - i) in
     (String.trim left, String.trim right)
 
-let lsplit (s : string) (c : char) : string * string = 
+let lsplit (s : string) (c : char) : string * string =
   split s c true
 let rsplit (s : string) (c : char) : string * string =
   split s c false
@@ -28,10 +28,18 @@ let rec starts_with_aux (s: string) (p : string) (i : int) =
   else if i >= String.length s then false
   else s.[i] = p.[i] && starts_with_aux s p (i + 1)
 (** [starts_with s p] checks if the string [p] matches the leading characters of [s] *)
-let starts_with (s : string) (p : string) = 
+let starts_with (s : string) (p : string) : bool =
   starts_with_aux s p 0
 
-(** [strip_suffix str] strips all characters after and including the 
+let str_reverse (s : string) : string =
+  let as_list = ref [] in
+  let () = String.iter (fun c -> as_list := c :: (!as_list)) s in
+  List.fold_left (fun s c -> s ^ (Char.escaped c)) "" (!as_list)
+
+let is_suffix (s : string) (p : string) : bool =
+  starts_with (str_reverse s) (str_reverse p)
+
+(** [strip_suffix str] strips all characters after and including the
  * rightmost period (.) *)
 let strip_suffix (filename : string) : string =
   if (String.contains filename '.')
@@ -43,20 +51,20 @@ let strip_suffix (filename : string) : string =
 let strip_trailing_slash (s : string) =
   let len = String.length s in
   if s.[len-1] = '/'
-  then String.sub s 0 (len - 1) 
+  then String.sub s 0 (len - 1)
   else s
 
-(** [strip_trailing_slash_all strs] remove the trailing slash from a 
+(** [strip_trailing_slash_all strs] remove the trailing slash from a
  * list of files *)
-let strip_trailing_slash_all (directories : string list) = 
+let strip_trailing_slash_all (directories : string list) =
   List.map (fun s -> strip_trailing_slash s) directories
 
 (** [tag_of_path p] strips all characters up to and including the rightmost / *)
-let tag_of_path (path : string) = 
-  if String.contains path '/' then 
+let tag_of_path (path : string) =
+  if String.contains path '/' then
     let i = 1 + String.rindex path '/' in
     String.sub path i ((String.length path) - i)
-  else 
+  else
     path
 
 (** [assert_file_exists f] raises [File_not_found] if [f] does not exist *)
@@ -66,7 +74,7 @@ let assert_file_exists (filename : string) : unit =
 
 (** [ensure_dir d] creates the directory [d] if it does not exist already. *)
 let ensure_dir (dir_name : string) =
-  if not (Sys.file_exists dir_name) then 
+  if not (Sys.file_exists dir_name) then
     Unix.mkdir dir_name 0o777
 
 (**
@@ -78,7 +86,7 @@ let ensure_dir (dir_name : string) =
  * This is the same as
  *   > cs3110 smoke dir/a dir/b dir/c
  *)
-let directories_of_list (dir : string) : string list = 
+let directories_of_list (dir : string) : string list =
   let dir = strip_trailing_slash dir in
   let len = String.length dir in
   (* Remove the leading @ to get a filename *)
@@ -91,7 +99,7 @@ let directories_of_list (dir : string) : string list =
   (* Return the fully-inferred list of directories *)
   List.rev (List.fold_left (fun acc name -> (prefix ^ name) :: acc) [] dir_names)
 
-(** [test_name_of_line s] extract the test name from a line printed by the 
+(** [test_name_of_line s] extract the test name from a line printed by the
  * inline test runner. Name should be the last 'word' of the string, separated
  * from everything else by a colon *)
 let test_name_of_line (line : string) : string =
@@ -116,17 +124,17 @@ let do_if_directory (dir : string)
                     (err_msg : string)
                     (exit_code : int) : 'a =
   if Sys.file_exists dir && Sys.is_directory dir then f dir
-  else let () = prerr_endline err_msg in exit exit_code 
+  else let () = prerr_endline err_msg in exit exit_code
 
 (** [filter_by_extension desired_extension files] returns a list of
     the files in [files] that have the desired extension. *)
 let filter_by_extension (desired_extension : string)
                         (files : string list) : string list =
-  let has_desired_extension file = 
+  let has_desired_extension file =
     begin match get_extension file with
     | None -> false
-    | Some ext -> ext = desired_extension 
-    end 
+    | Some ext -> ext = desired_extension
+    end
   in
   List.filter has_desired_extension files
 
@@ -137,4 +145,3 @@ let get_files_with_extension (desired_extension : string)
   let get_files dir =
     filter_by_extension desired_extension (Array.to_list (Sys.readdir dir)) in
   try get_files dir with _ -> []
-
