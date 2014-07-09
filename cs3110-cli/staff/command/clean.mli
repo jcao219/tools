a6348880d6386f33483cfd620af9cbdb840925cd
Ben <ben@rentenna.com>
2014-07-08 23:19:30 -0400

    [cli #44 #55] core pipeline, no more string parsing Sys.argv

--- cs3110-cli/staff/command/clean.mli
+++ cs3110-cli/staff/command/clean.mli
@@ -1,8 +1,1 @@
-val compile : unit -> unit
-val diff : unit -> unit
-val email : unit -> unit
-val harness : unit -> unit
-val smoke : unit -> unit
-val clean : string list -> unit -> unit
-val clean_command : Core.Std.Command.t
-val run_clean_command : unit -> unit
+val command : Core.Std.Command.t
