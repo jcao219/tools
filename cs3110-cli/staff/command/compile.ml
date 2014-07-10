a7b21db0db2b3a01c5f456c402850d287856c03b
Ben <ben@rentenna.com>
2014-07-10 14:15:13 -0400

    [cli-compile] abstracted compiler options a little

--- cs3110-cli/staff/command/compile.ml
+++ cs3110-cli/staff/command/compile.ml
@@ -4,6 +4,27 @@ open Io_util
 open Filepath_util
 open Process_util
 
+(* Default compiler flags. These are Jane Street's settings.
+   [-warn-error] says "treat the following warnings as errors.
+   [+a] means "all warnings"
+   [-4-33-34-39-40-41-42-43-44] means "except
+   - [4]  : Fragile pattern matching (uses _)
+   - [33] : Unused open statement
+   - [34] : Unused type declaration
+   - [39] : Unused rec flag
+   - [40] : Constructor or label name used out of scope
+   - [41] : Ambiguous constructor label or name
+   - [42] : Disambiguated constructor label or name
+   - [43] : Nonoptional label applied as optional
+   - [44] : Open statement shadows an identifier
+   Note that warnings 40-45 are for [ocamlopt] and not for [ocamlc].
+   See more documentation here: http://caml.inria.fr/pub/docs/manual-ocaml/native.html
+ *)
+let default_compiler_flags = [
+  "-w";
+  "+a-4-33-34-40-41-42-43-44";
+]
+
 (* 2014-04-18: "ocamlbuild must be invoked from the root of the project"
  * http://nicolaspouillard.fr/ocamlbuild/ocamlbuild-user-guide.html *)
 let assert_ocamlbuild_friendly_filepath (path : string) : unit =
@@ -14,6 +35,16 @@ let assert_ocamlbuild_friendly_filepath (path : string) : unit =
   let is_absolute = String.contains path '~' || path.[0] = '/' in
   if (is_absolute || is_relative)
   then raise (Invalid_filepath "Must call cs3110 from the project root. Absolute or relative paths are not allowed.")
+
+(** [format_compiler_flags fs] prepend the string '-cflag' to each compiler
+    flag for compliance with ocamlbuild *)
+let format_compiler_flags (flags : string list) : string list =
+  List.fold_right flags ~f:(fun f acc -> "-cflag":: f :: acc) ~init:[]
+
+let get_compiler_flags () : string list =
+  (* TODO check init file *)
+  let fs = default_compiler_flags in
+  format_compiler_flags fs
 
 (* TODO change these constants *)
 (** [get_dependencies ()] read in config file,
@@ -41,7 +72,7 @@ let get_opam_packages () : string list = cSTD_OPAM_PACKAGES @
 
 (** [compile args main] compile [main] into a bytecode executable.
     Relies on ocamlbuild. *)
-let compile (run_quiet:bool) (main_module : string) : unit =
+let compile (run_quiet:bool) (main_module : string) : int =
   let () =
     assert_ocamlbuild_friendly_filepath main_module;
     assert_file_exists (main_module);
@@ -54,14 +85,6 @@ let compile (run_quiet:bool) (main_module : string) : unit =
   in
   let target = Format.sprintf "%s.d.byte" (strip_suffix main_module) in
   let ocamlbuild_flags =  [
-    "-cflag";
-    "-warn-error";
-    "-cflag";
-    "+a";                       (* treat the default warnings as errors *)
-    "-cflag";
-    "-w";
-    "-cflag";
-    "A-4-33-40-41-42-43-34-44"; (* Jane street's warnings as errors *)
     "-use-ocamlfind";
     "-no-links";
     "-tag-line"; "<*.ml{,i}> : syntax(camlp4o), " ^ opam_packages_str;
@@ -69,10 +92,13 @@ let compile (run_quiet:bool) (main_module : string) : unit =
     "-tag-line"; "<*.native> : " ^ opam_packages_str;
     target
   ] in
-  check_code (run_process "ocamlbuild" (
+  let quiet = if run_quiet then ["-quiet"] else [] in
+  let compiler_args = (
     (get_dependencies ()) @
     (get_libraries ())    @
-    if run_quiet then "-quiet"::ocamlbuild_flags else ocamlbuild_flags))
+    (get_compiler_flags ()) @ quiet @ ocamlbuild_flags)
+  in
+  run_process "ocamlbuild" compiler_args
 
 let command =
   Command.basic
@@ -88,4 +114,4 @@ let command =
       empty
       +> flag "-q" no_arg ~doc:"Run quietly."
       +> anon ("filename" %: string))
-    (fun quiet target () -> compile quiet target)
+    (fun quiet target () -> check_code (compile quiet target))
