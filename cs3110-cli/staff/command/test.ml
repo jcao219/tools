acc553f2d149179961546890616f826ddeaf6695
Ben <ben@rentenna.com>
2014-07-11 14:00:06 -0400

    [cli-test] fast and dirty port, main functionality in place

--- cs3110-cli/staff/command/test.ml
+++ cs3110-cli/staff/command/test.ml
@@ -1,27 +1,42 @@
-open Cli_constants
+open Core.Std
 open Filepath_util
 
-(* The general form of the test command *)
-let test_parameterized (main_module : string) (output : string) : int =
-  let exec = Format.sprintf "_build/%s.d.byte" main_module in
-  let _ = assert_file_exists exec in
-  let cmd = Format.sprintf "%s inline-test-runner dummy -log %s" exec output in
-  Sys.command cmd
+let test ?(quiet=false) ?output (main_module : string) : int =
+  let build_dir = "_build" in (* TODO abstract this *)
+  let main_module = strip_suffix main_module in
+  (* TODO clean this the fuck up *)
+  let exec = Format.sprintf "%s/%s.d.byte" build_dir main_module in
+  let base_cmd = [exec; "inline-test-runner"; "dummy"] in
+  let cmd = begin match output with
+    | Some dest ->
+      if quiet
+      then base_cmd @ ["2>& 1>/dev/null | grep '^File' >"; dest]
+      else base_cmd @ ["&>"; dest]
+    | None      ->
+      if quiet
+      then base_cmd @ [">"; "/dev/null"]
+      else base_cmd
+  end in
+  (* TODO print message if all pass *)
+  Sys.command (String.concat ~sep:" " cmd)
 
-(* Redirects stderr to stdout's destination, changes stdout's 
- * destination to null, saves the error messages to a file *)
-let test_logging_errors (main_module :string) : int =
-  (* 2014-01-11: Steals only the first line of output for each error *)
-  let dest = "2>& 1>/dev/null | grep '^File' > " ^ cFAIL_OUTPUT in
-  test_parameterized main_module dest
-
-let test_quiet (main_module : string) =
-  test_parameterized main_module "> /dev/null"
-
-(** [test file] executes the unit tests within [file].
- * Inner workings documented here: [https://github.com/janestreet/pa_ounit]
- * Standard output is ignored; exceptions still print.
- * This is the default and preferred way of running test. *)
-let test (main_module : string) : int =
-  test_quiet main_module
-
+let command =
+  Command.basic
+    ~summary:"Checks unit tests in a compiled executable."
+    ~readme:(fun () -> String.concat ~sep:"\n" [
+      "[cs3110 test] dispatches the inline unit tests within a compiled";
+      "executable. Test results are printed to the console."
+    ])
+    Command.Spec.(
+      empty
+      +> flag ~aliases:["-r"] "--recompile" no_arg ~doc:"Compile target before testing."
+      +> flag ~aliases:["-q"] "--quiet" no_arg ~doc:"Run quietly. Do not print debug statements."
+      +> flag ~aliases:["-o"] "--output" (optional string) ~doc:"Save test output to a file."
+      +> anon ("target" %: string)
+    )
+    (fun recompile quiet output main () ->
+      (* let () = if recompile then Command.compile main in *)
+      Process_util.check_code (begin match output with 
+      | Some dest -> test ~quiet:quiet ~output:dest main
+      | None -> test ~quiet:quiet main end)
+    )
