7af5e011ef5383fee282525170941a7a7b0c929e
Ben <ben@rentenna.com>
2014-07-16 16:19:55 -0400

    [cli-smoke] targets option, more docs

--- cs3110-cli/staff/command/smoke.ml
+++ cs3110-cli/staff/command/smoke.ml
@@ -11,6 +11,8 @@ type options = {
   verbose             : bool;
 }
 
+(** [make_email_message n fs] Create the body of an email to send to student [n]
+    because his/her files [fs] failed to compile. *)
 let make_email_message (netid : string) (failed_targets : string list) : string list =
   let prefix = "* " in
   [
@@ -22,12 +24,18 @@ let make_email_message (netid : string) (failed_targets : string list) : string 
     "--- Automatically generated message from the CS3110 test harness ---";
   ]
 
-(** [copy_directory o d] save a copy of the directory [dir] for later diffing.
+(** [copy_directory o n d] save a copy of the directory [dir] for later diffing.
     The options [o] specify where to save the copy. *)
 let copy_directory (opts : options) (netid : string) (dir : string) : unit =
   let existing_dir = Format.sprintf "%s/%s" opts.nocompile_directory netid in
-  let () = if opts.verbose then Format.printf "[smoke] removing old diff directory '%s'.\n" existing_dir in
-  let () = ignore (Sys.command (Format.sprintf "rm -rf %s" existing_dir)) in
+  let () =
+    begin match Sys.file_exists existing_dir with
+      | `No | `Unknown -> ()
+      | `Yes           ->
+        let () = if opts.verbose then Format.printf "[smoke] removing old diff directory '%s'.\n" existing_dir in
+        ignore (Sys.command (Format.sprintf "rm -rf %s" existing_dir))
+    end
+  in
   let () = if opts.verbose then Format.printf "[smoke] copying directory '%s' to folder '%s'.\n" dir opts.nocompile_directory in
   let exit_code = Sys.command (Format.sprintf "cp -r %s %s" dir opts.nocompile_directory) in
   let () = if exit_code <> 0 then Format.printf "[smoke] ERROR failed to save directory '%s'\n" dir in
@@ -99,19 +107,23 @@ let smoke (opts : options) (directories : string list) : unit =
 
 (** [get_smoke_targets o] figure out which files to compile. This could be
     set in the config file or given as a command line option. *)
-let get_smoke_targets () : string list =
-  begin match Sys.file_exists cSMOKE_TARGETS with
-    | `Yes           ->
-       List.map ~f:strip_suffix (read_lines (open_in cSMOKE_TARGETS))
-    | `No | `Unknown ->
-       begin match Sys.file_exists cTESTS_DIR with
-         | `Yes -> List.map
-                     ~f:(fun f -> fst (rsplit f '_'))
-                     (List.filter
-                       ~f:is_valid_test_file
-                       (Array.to_list (Sys.readdir cTESTS_DIR))
-                     )
-         | `No | `Unknown -> raise (File_not_found cSMOKE_TARGETS)
+let get_smoke_targets (tgts : string list) : string list =
+  begin match tgts with
+    | _::_ -> tgts
+    | []   ->
+       begin match Sys.file_exists cSMOKE_TARGETS with
+         | `Yes           ->
+            List.map ~f:strip_suffix (read_lines (open_in cSMOKE_TARGETS))
+         | `No | `Unknown ->
+            begin match Sys.file_exists cTESTS_DIR with
+              | `Yes -> List.map
+                          ~f:(fun f -> fst (rsplit f '_'))
+                          (List.filter
+                             ~f:is_valid_test_file
+                             (Array.to_list (Sys.readdir cTESTS_DIR))
+                          )
+              | `No | `Unknown -> raise (File_not_found cSMOKE_TARGETS)
+            end
        end
   end
 
@@ -122,22 +134,22 @@ let command =
       "The smoke test is a sanity check for students. We make sure their submissions compile.";
       "If not, we generate an email that can be sent with [cs3110 email] and save a record of";
       "the submission. The policy is that students can resubmit a trivial fix with no penalty.";
-      "Use [cs3110 diff] to compare saved copies with resubmissions."
+      "Use [cs3110 diff] to compare saved copies with resubmissions. Specify which files to";
+      "compile with the [-target] option or the config file.";
     ])
     Command.Spec.(
       empty
       +> flag ~aliases:["-v"] "-verbose" no_arg ~doc:" Print debugging information."
-      (* +> flag target *)
-      (* +> flag targets *)
+      +> flag ~aliases:["-t"] "-target" (listed string) ~doc:"target Attempt to compile TARGET in each SUBMISSION directory."
       +> anon (sequence ("submission" %: string))
     )
-    (fun v subs () ->
+    (fun v tgts subs () ->
      let () = ensure_dir cEMAIL_DIR in
      let () = ensure_dir cNOCOMPILE_DIR in
      let opts = {
        email_directory     = cEMAIL_DIR;
        nocompile_directory = cNOCOMPILE_DIR;
-       targets             = get_smoke_targets ();
+       targets             = get_smoke_targets tgts;
        verbose             = v;
      } in
      smoke opts (at_expand subs))
