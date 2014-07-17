5be803f7918b8f7dd61c8fa1e7f99fe773d324ba
Ben <ben@rentenna.com>
2014-07-16 21:40:25 -0400

    [cli-harness] the harness's interface to the world

--- cs3110-cli/staff/command/harness.mli
+++ cs3110-cli/staff/command/harness.mli
@@ -1,3 +1,1 @@
-val run : string -> string list -> unit
-(** [run t ds] Execute the tests stored in directory [t] on
- * all implementation directories [ds]. Log lots of output *)
+val command : Core.Std.Command.t
