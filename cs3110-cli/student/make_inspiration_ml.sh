eda969174f989441353ecc5093c23ccc95055dea
Drew Weymouth <dweymouth@users.noreply.github.com>
2014-07-23 23:57:22 -0700

    shell script to generate inspiration.ml from fortune file

---
+++ cs3110-cli/student/make_inspiration_ml.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+gawk '
+BEGIN { RS="\0" }
+{
+	print "let inspirations = [|";
+	n = split($0, inspirations, /%\n/);
+	for (i = 1; i <= n; i++) {
+		ocamlstr = gensub(/"/, "\\\\\"", "g", inspirations[i]);
+		ocamlstr = gensub(/\n/, "\\\\n", "g", ocamlstr);
+		print "\"" ocamlstr "\";";
+	}
+	print "|] in";
+
+	print "let _ = Random.self_init () in"
+	print "let rand_idx = Random.int (Array.length inspirations) in"
+	print "print_string inspirations.(rand_idx)"
+}
+' < inspirations.fortune > inspiration.ml
