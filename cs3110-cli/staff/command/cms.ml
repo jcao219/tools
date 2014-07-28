179d90f79a0e3a0814a7af4902e5108815fc5f7a
Ben Greenman <greenman.b@husky.neu.edu>
2014-07-27 23:24:07 -0400

    [cli-cms] checkpoint, it all compiles

--- cs3110-cli/staff/command/cms.ml
+++ cs3110-cli/staff/command/cms.ml
@@ -7,80 +7,29 @@ exception Invalid_spreadsheet of string
 
 module StringSet = Set.Make(String)
 type options = {
-  columns            : StringSet.t;
-  delimeter          : char;
-  input_spreadsheet  : string;
+  column_names       : StringSet.t;
+  delimiter          : char;
+  input_comments     : string;
   output_spreadsheet : string;
   verbose            : bool;
 }
 
-(* (\* [get_columns s] Collect the capitalized columns from the string [s]. *)
-(*  * Ignore the CMS-specific [Totals] column. *\) *)
-(* let get_columns (s : string) : string list * int list = *)
-(*   (\* Split line by comma, fold over titles *\) *)
-(*   begin match Str.split (Str.regexp ",") s with *)
-(*     | [] -> raise (Invalid_spreadsheet "Cannot generate grades table from empty sheet.") *)
-(*     | "NetID"::tail -> *)
-(*       let titles, indices, _ = *)
-(*         List.fold_left (fun (ts,is,i) title -> *)
-(*           let t_code = Char.code title.[0] in *)
-(*           if (Char.code 'A' <= t_code) && (t_code <= Char.code 'Z') && (title <> "Total") *)
-(*           then (title :: ts, i::is, i+1) *)
-(*           else (ts,is,i+1) *)
-(*         ) (["NetID"],[],1) tail *)
-(*       in *)
-(*       (List.rev ("Add Comments" :: titles)), List.rev indices *)
-(*     | _::_ -> raise (Invalid_spreadsheet "First column MUST be 'NetID'") *)
-(*   end *)
+module LabeledRow = Set.Make(struct
+  type t = string * string
+  let compare (l1,_) (l2,_) = Pervasives.compare l1 l2
+  let sexp_of_t _ = failwith "not implemented"
+  let t_of_sexp _ = failwith "not implemented"
+end)
 
-(* let get_comments (netid : string) : string = *)
-(*   let fname = Format.sprintf "%s/%s.md" cOUTPUT_DIR netid in *)
-(*   if (Sys.file_exists fname) *)
-(*   then (",\"" ^ (String.concat " \n " (read_lines (open_in fname))) ^ "\"") *)
-(*   else "," *)
-
-(* (\** [run f] Parse the spreadsheet [f]. *)
-(*  * Save the columns denoted with capital letters (but not the overall total) *)
-(*  * Add comments using the files in [cOUTPUT_DIR]. *\) *)
-(* let run (fname : string) : unit = *)
-(*   let in_chn = open_in fname in *)
-(*   let out_chn = open_out cCMS_FINAL in *)
-(*   (\* Get titles from first line *\) *)
-(*   let columns, indices = get_columns (input_line in_chn) in *)
-(*   Format.printf "INDICES = [%s]\n" (String.concat "; " (List.map string_of_int indices)); *)
-(*   (\* Write titles to output *\) *)
-(*   let () = output_string out_chn (String.concat "," columns) in *)
-(*   let () = output_string out_chn "\n" in *)
-(*   (\* Get netids and scores from remaining lines, *)
-(*    * write these to the output *\) *)
-(*   fold_in_channel (fun _ line -> *)
-(*     (\* Iterate over entries in row. Track current index and title indices so-far-popped. *\) *)
-(*     let netid, tail = lsplit line ',' in *)
-(*     let () = output_string out_chn netid in *)
-(*     let _ = *)
-(*       List.fold_left (fun (curr_i, ixs) score -> *)
-(*         begin match ixs with *)
-(*           | [] -> *)
-(*             (curr_i+1, ixs) *)
-(*           | i::xs when i = curr_i -> *)
-(*             let () = output_string out_chn (","^score) in *)
-(*             (curr_i+1, xs) *)
-(*           | _::_ -> *)
-(*             (curr_i+1, ixs) *)
-(*         end *)
-(*       ) (1, indices) (Str.split (Str.regexp ",") tail) *)
-(*     in *)
-(*     let () = output_string out_chn (get_comments netid) in *)
-(*     output_string out_chn "\n" *)
-(*   ) () in_chn *)
+type cms_row = string * LabeledRow.t * string
 
 (** [get_titles_exn ~sep sheet] Return the first line from the file [sheet],
     split into a list on the character [~sep]. Raise an exception in the
     file [sheet] is empty. *)
-let get_first_line (sheet : string) : string list =
-  let input    = In_channel.create sheet in
-  let line_opt = In_channel.input_line input in
-  let ()       = In_channel.close sheet in
+let get_titles_exn ~sep (sheet : string) : string list =
+  let in_chn   = In_channel.create sheet in
+  let line_opt = In_channel.input_line in_chn in
+  let ()       = In_channel.close in_chn in
   begin match line_opt with
     | Some ln -> String.split ~on:sep ln
     | None    ->
@@ -88,18 +37,68 @@ let get_first_line (sheet : string) : string list =
        raise (Invalid_spreadsheet msg)
   end
 
+(** [parse_comments o n] Convert the comments file for netid [n] into
+    a spreadsheet-ready string. *)
+let parse_comments (opts : options) (netid : string) : string =
+  let fname = Format.sprintf "%s/%s.md" opts.input_comments netid in
+  begin match Sys.file_exists fname with
+    | `No | `Unknown -> ""
+    | `Yes           ->
+       let lines = In_channel.read_lines fname in
+       Format.sprintf "\"%s\"" (String.concat ~sep:" \n " lines)
+  end
+
+(** [parse_row_exn opts ~titles line] Read the data values from a row [line] of the spreadsheet.
+    Pull out the netid and filter the data columns and read the comments from an external file. *)
+let parse_row_exn (opts : options) ~titles (line : string) : cms_row =
+  let ()       = if opts.verbose then Format.printf "[cms] Reading line '%s'.\n" line in
+  let data     = String.split ~on:opts.delimiter line in
+  let ()       = if opts.verbose then Format.printf "[cms] Matching values with titles.\n" in
+  let ()       = if not ((List.length titles) = (List.length data)) then raise (Invalid_spreadsheet "Row does not match titles") in
+  let lr       = List.fold2_exn titles data
+                   ~f:(fun acc t d -> LabeledRow.add acc (t,d))
+                   ~init:LabeledRow.empty
+  in
+  let ()       = if opts.verbose then Format.printf "[cms] Extracting netid...\n" in
+  let netid    = snd (LabeledRow.find_exn ~f:(fun (t,_) -> t = "NetId") lr) in
+  let ()       = if opts.verbose then Format.printf "[cms] Filtering columns...\n" in
+  let scores   = LabeledRow.filter ~f:(fun (t,_) -> List.mem titles t) lr in
+  let ()       = if opts.verbose then Format.printf "[cms] Reading comments...\n" in
+  let comments = parse_comments opts netid in
+  (netid, scores, comments)
+
 (** [cms o sheet] Read the spreadsheet [sheet] and extract
     particular columns. Save these columns along with
     harness-generated comments in a new spreadsheet. *)
 let cms (opts : options) (sheet : string) =
-  (* TODO we're gonna want a spreadsheet functor. One or two?
-          because we could parse input line-by line or read the
-          whole file.
-          I'm thinking we should work with the functor, and later improve
-          that....
-          AH, wait. Can't define an input functor. Okay, that makes things easy.
-   *)
-  failwith ""
+  let () = if opts.verbose then Format.printf "[cms] Creating spreadsheet template...\n" in
+  let module CmsSpreadsheet = Spreadsheet.Make(struct
+      type row                          = cms_row
+      let compare_row (l1,_,_) (l2,_,_) = Pervasives.compare l1 l2
+      let filename                      = opts.output_spreadsheet
+      let row_of_string _               = failwith "CMS.row_of_string intentionally not implemented"
+      let string_of_row (id, lrow, cs)  = let scores = LabeledRow.fold_right ~f:(fun (_,s) acc -> s::acc) ~init:[] lrow in
+                                          String.concat ~sep:(Char.escaped opts.delimiter) ([id] @ scores @ [cs])
+      let title                         = let names = StringSet.to_list opts.column_names in
+                                          String.concat ~sep:(Char.escaped opts.delimiter) (["NetId"] @ names @ ["Add Comments"])
+    end)
+  in
+  let ()        = if opts.verbose then Format.printf "[cms] Parsing input file '%s'...\n" sheet in
+  let in_chn    = In_channel.create sheet in
+  let ()        = if opts.verbose then Format.printf "[cms] Reading & saving column headers.\n" in
+  let titles    = String.split ~on:opts.delimiter (Option.value_exn (In_channel.input_line in_chn)) in
+  let ()        = if opts.verbose then Format.printf "[cms] Reading & parsing spreadsheet body.\n" in
+  let cms_sheet = In_channel.fold_lines in_chn
+                    ~f:(fun acc ln -> CmsSpreadsheet.add_row acc ~row:(parse_row_exn opts ~titles:titles ln))
+                    ~init:(CmsSpreadsheet.create ())
+  in
+  let ()        = In_channel.close in_chn in
+  let () = if opts.verbose then Format.printf "[cms] Finished! Saving results to '%s'.\n" opts.output_spreadsheet in
+  CmsSpreadsheet.write cms_sheet
+
+(** [is_capitalized s] True if string [s] begins with a capital A-Z letter. *)
+let is_capitalized (str : string) : bool =
+  String.length str > 0 && ('A' <= str.[0] && str.[0] <= 'Z')
 
 (** [infer_columns ~sep sheet] Infer the special column names
     from the spreadsheet [sheet]. Prompt user to validate selection. *)
@@ -107,15 +106,15 @@ let infer_columns ~sep (sheet : string) : StringSet.t =
   let titles = get_titles_exn ~sep:sep sheet in
   List.fold_left (* Save capitalized titles *)
     ~f:(fun acc str ->
-        if String.length str > 0 && ('A' <= str.[0] && str.[0] <= 'Z')
+        if (is_capitalized str) && (str <> "NetId")
         then StringSet.add acc str
         else acc)
     ~init:StringSet.empty
     titles
 
-(** [infer_delimeter f] Infer the delimeter string from a spreadsheet
+(** [infer_delimiter f] Infer the delimiter string from a spreadsheet
     file by reading the extension. *)
-let infer_delimeter (fname : string) : string =
+let infer_delimiter (fname : string) : char =
   begin match get_extension fname with
     | Some "csv" -> ','
     | Some "tab" -> '\t'
@@ -148,7 +147,7 @@ let command =
       empty
       +> flag ~aliases:["-v"] "-verbose" no_arg            ~doc:" Print debugging information."
       +> flag ~aliases:["-i"] "-input"   (optional file)   ~doc:"DIR Specify the directory of harness-generated comments."
-      +> flag ~aliases:["-s"] "-sep"     (optional string) ~doc:"STR Delimeter character (i.e. comma or tab) for the input sheet."
+      +> flag ~aliases:["-s"] "-sep"     (optional string) ~doc:"STR Delimiter character (i.e. comma or tab) for the input sheet."
       +> flag ~aliases:["-o"] "-output"  (optional file)   ~doc:"FILE Write output to the file FILE."
       +> flag ~aliases:["-c"] "-column"  (listed string)   ~doc:"NAME Scrape the column titled NAME from the input spreadsheet."
       +> anon ("spreadsheet" %: string)
@@ -159,18 +158,20 @@ let command =
       let input = Option.value inp ~default:cHARNESS_SHEET in (* TODO remove constants *)
       let delim =  begin match sep with
                       | Some s -> Char.of_string s (* TODO error handling *)
-                      | None   -> infer_delimeter sheet
+                      | None   -> infer_delimiter sheet
                    end in
-      let ()    = if v then Format.printf "[cms] Identified delimeter '%c'.\n" delim in
+      let ()    = if v then Format.printf "[cms] Identified delimiter '%c'.\n" delim in
+      let ()    = if not ("NetId" = List.hd_exn (get_titles_exn ~sep:delim sheet))
+                  then raise (Invalid_spreadsheet "First column should be 'NetId'.") in
       let cols  = begin match cols with
                       | []   -> infer_columns    ~sep:delim input
                       | _::_ -> validate_columns ~sep:delim ~sheet:input cols
                   end in
       let ()    = if v then Format.printf "[cms] Target columns are [%s].\n" (String.concat ~sep:"; " (StringSet.to_list cols)) in
       let opts  = {
-        columns            = cols;
-        delimeter          = delim;
-        input_spreadsheet  = input;
+        column_names       = cols;
+        delimiter          = delim;
+        input_comments     = input;
         output_spreadsheet = Option.value out ~default:"./cms_spreadsheet.csv";
         verbose            = v;
       } in
