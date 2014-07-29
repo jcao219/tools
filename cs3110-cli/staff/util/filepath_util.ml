45946050a8290c1d0748333253eebfeebe2595ea
Ben Greenman <greenman.b@husky.neu.edu>
2014-07-29 02:34:42 -0400

    [cli-utils] all Core now

--- cs3110-cli/staff/util/filepath_util.ml
+++ cs3110-cli/staff/util/filepath_util.ml
@@ -1,33 +1,37 @@
 open Cli_constants
-open Io_util
+open Core.Std
 
 (** [strip_suffix str] strips all characters after and including the
  * rightmost period (.) *)
-let strip_suffix (filename : string) : string =
-  if (String.contains filename '.')
-  then String.sub filename 0 (String.rindex filename '.')
-  else filename
+let strip_suffix (str : string) : string =
+  begin match String.rsplit2 ~on:'.' str with
+    | Some (hd,_) -> hd
+    | None        -> str
+  end
 
-(** [strip_trailing_slash str] removes the last character of [s],
- * but only if that character is a / *)
-let strip_trailing_slash (s : string) =
-  let len = String.length s in
-  if s.[len-1] = '/'
-  then String.sub s 0 (len - 1)
-  else s
+(** [strip_trailing_slash str] removes the last character of [str],
+    but only if that character is a / *)
+let strip_trailing_slash (str : string) : string =
+  begin match String.rsplit2 ~on:'/' str with
+    | Some (hd, "") -> hd
+    | Some _ | None -> str
+  end
 
-(** [assert_file_exists f] raises [File_not_found] if [f] does not exist *)
-let assert_file_exists ?msg (filename : string) : unit =
-  if not (Sys.file_exists filename) then
-    raise (File_not_found (Core.Std.Option.value msg ~default:filename))
+(** [assert_file_exists fname] raises [File_not_found] if [f] does not exist.
+    Optional argument specifies the error message. *)
+let assert_file_exists ?msg (fname : string) : unit =
+  begin match Sys.file_exists fname with
+    | `Yes           -> ()
+    | `No | `Unknown -> raise (File_not_found (Option.value msg ~default:fname))
+  end
 
 (** [is_ml f] True if file [f] is a .ml file. *)
 let is_ml (fname : string) : bool =
-  Core.Std.String.is_suffix fname ~suffix:".ml"
+  String.is_suffix fname ~suffix:".ml"
 
 (** [is_mli f] True if file [f] is a .mli file. *)
 let is_mli (fname : string) : bool =
-  Core.Std.String.is_suffix fname ~suffix:".mli"
+  String.is_suffix fname ~suffix:".mli"
 
 (** [ensure_ml f] Check if [f] has a '.ml' suffix. If not, append one. *)
 let ensure_ml (fname : string) : string =
@@ -36,27 +40,27 @@ let ensure_ml (fname : string) : string =
   else fname ^ ".ml"
 
 (** [ensure_dir d] creates the directory [d] if it does not exist already. *)
-let ensure_dir (dir_name : string) =
-  if not (Sys.file_exists dir_name) then
-    Unix.mkdir dir_name 0o777
+let ensure_dir (dir_name : string) : unit =
+  begin match Sys.file_exists dir_name with
+    | `No | `Unknown -> Unix.mkdir dir_name (* default perm is 0777 *)
+    | `Yes           -> ()
+  end
 
-(**
- * [directories_of_list dir] Build a list of directories from the file [dir].
- * [dir] should contain a newline-separated list of directories.
- * Example: to test files { dir/a , dir/b , dir/c }, you may
- *   > cat "a\nb\nc" > dir/files
- *   > cs3110 smoke @dir/files
- * This is the same as
- *   > cs3110 smoke dir/a dir/b dir/c
- *)
+(** [directories_of_list dir] Build a list of directories from the file [dir].
+    The directory [dir] should contain a newline-separated list of filenames. *)
 let directories_of_list (fname : string) : string list =
   let () = assert_file_exists fname in
   (* Argument is @path/to/list-of-directory-names.    *
    * Extract a list of directory names from the list. *)
-  let dir_names = read_lines (open_in fname) in
-  let prefix = String.sub fname 0 (String.rindex fname '/') in
-  (* Return the fully-inferred list of directories *)
-  List.rev (List.fold_left (fun acc name -> (Format.sprintf "%s/%s" prefix name) :: acc) [] dir_names)
+  let dir_names = In_channel.read_lines fname in
+  let prefix    = begin match String.rsplit2 ~on:'/' fname with
+                    | Some (prefix,_) -> prefix
+                    | None            -> ""
+                  end
+  in
+  List.fold_right dir_names
+    ~f:(fun name acc -> (Format.sprintf "%s/%s" prefix name) :: acc)
+    ~init:[]
 
 (** [at_expand dirs] optionally expand a file containing a list into a list of directories.
     If the input is a singleton list where the first element is prefixed by and '@' character,
@@ -65,18 +69,20 @@ let directories_of_list (fname : string) : string list =
     Else return the input unchanged. I would have named it '@_expand' but that didn't compile. *)
 let at_expand (dirs : string list) : string list =
   begin match dirs with
-    | [fname] when (0 < String.length fname) && fname.[0] = '@' ->
-       (* Remove leading '@' *)
-       directories_of_list (String.sub fname 1 ((String.length fname) - 1))
-    | [] | _::_ -> dirs
+    | []       -> dirs
+    | fname::_ ->
+       begin match String.lsplit2 ~on:'@' fname with
+         | Some ("", tl) -> directories_of_list tl
+         | Some _ | None -> dirs
+       end
   end
 
 (** [get_extension file_name] gets the extension of the file
- *  [file_name]. The extension is defined to be the characters
- *  occuring to the right of the right-most occurence of the '.'
- *  character. *)
+    [file_name]. The extension is defined to be the characters
+    occuring to the right of the right-most occurence of the '.'
+    character. *)
 let get_extension (file_name : string) : string option =
-  begin match Core.Std.String.rsplit2 ~on:'.' file_name with
+  begin match String.rsplit2 ~on:'.' file_name with
     | Some (_,ext) -> Some ext
     | None         -> None
   end
@@ -84,9 +90,8 @@ let get_extension (file_name : string) : string option =
 (** [filter_directory ~p d] Read files in directory [d], remove the files not matching the
     predicate [p]. *)
 let filter_directory ~f (dir : string) : string list =
-  (* TODO catch exception / check if directory exists. *)
   let all_files = Sys.readdir dir in
-  Core.Std.Array.fold_right all_files
+  Array.fold_right all_files
     ~f:(fun x acc -> if f x then x :: acc else acc)
     ~init:[]
 
@@ -96,7 +101,7 @@ let filter_directory ~f (dir : string) : string list =
     Given 'file.ml', this function is the identity. *)
 let filename_of_path (path : string) : string =
   let path = strip_trailing_slash path in
-  begin match Core.Std.String.rsplit2 ~on:'/' path with
+  begin match String.rsplit2 ~on:'/' path with
     | Some (_,str) -> str
     | None         -> path
   end
@@ -106,8 +111,15 @@ let filename_of_path (path : string) : string =
 let soft_copy (dir1 : string) (dir2 : string) : int =
   Sys.command (Format.sprintf "cp -r -n %s/. %s" dir1 dir2)
 
-let files_exist (files : string list) : bool =
-  List.fold_left (fun acc f -> acc && Sys.file_exists f) true files
+let rec files_exist (files : string list) : bool =
+  begin match files with
+    | []     -> true
+    | hd::tl ->
+       begin match Sys.file_exists hd with
+         | `No | `Unknown -> false
+         | `Yes           -> files_exist tl
+       end
+  end
 
 (** [check_installed cmd] False is the unix tool [cmd] is not found. *)
 let check_installed (cmd : string) : bool =
