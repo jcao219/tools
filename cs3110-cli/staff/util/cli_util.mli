8aeb7a82551a4c72c272e8741dd9f6cc3f8730fe
Ben Greenman <greenman.b@husky.neu.edu>
2014-07-31 06:03:42 -0400

    [cli-config] deleted comments from util.mli

--- cs3110-cli/staff/util/cli_util.mli
+++ cs3110-cli/staff/util/cli_util.mli
@@ -5,11 +5,6 @@ exception Command_not_found of string
 exception File_not_found of string
 exception Invalid_filepath of string
 
-(* TODO delete one *)
-(* module StringSet : sig include (module type of Core.Std.Set.Make(String)) *)
-(*                        val sexp_of_t = Sexp.of_string *)
-(*                        val sexp_to_t = Sexp.to_string *)
-(*                    end *)
 module StringSet : Core.Std.Set.S with type Elt.t = string
 
 (** [assert_file_exists fname] raises [File_not_found] if [f] does not exist.
